5c2d2c7b3eb14d4000dc703af071cd0cfb7c8e5b
Elb1to <elbitomc@gmail.com>
2020-09-25 19:45:56 -0500

    Registered and instated more stuff
    * Kit loading and saving on startup, shutdown
    * Registered FrozedGkitsCommand
    * Added "copyright" method.

--- src/main/java/club/frozed/gkit/FrozedGKits.java
+++ src/main/java/club/frozed/gkit/FrozedGKits.java
@@ -1,11 +1,16 @@
 package club.frozed.gkit;
 
+import club.frozed.gkit.commands.FrozedGkitsCommand;
 import club.frozed.gkit.commands.GkitCommand;
 import club.frozed.gkit.commands.ManageCommand;
+import club.frozed.gkit.managers.KitManager;
+import club.frozed.gkit.utils.chat.Color;
 import club.frozed.gkit.utils.command.CommandFramework;
 import club.frozed.gkit.utils.config.FileConfig;
+import club.frozed.gkit.utils.menu.ButtonListener;
 import lombok.Getter;
 import lombok.Setter;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 @Getter @Setter
@@ -15,21 +20,39 @@ public final class FrozedGKits extends JavaPlugin {
     private CommandFramework commandFramework;
 
     private FileConfig pluginConfig, kitsConfig;
+    private KitManager kitManager;
 
     @Override
     public void onEnable() {
         instance = this;
-        commandFramework = new CommandFramework(this);
 
+        if (!this.getDescription().getAuthors().contains("Elb1to") || !this.getDescription().getAuthors().contains("Ryzeon") ||
+                !this.getDescription().getAuthors().contains("FrozedClubDevelopment") || !this.getDescription().getWebsite().contains("www.frozed.club") ||
+                !this.getDescription().getName().equals("FrozedGKits")) {
+            Bukkit.getPluginManager().disablePlugins();
+        }
+
+        commandFramework = new CommandFramework(this);
         pluginConfig = new FileConfig(this, "config.yml");
         kitsConfig = new FileConfig(this, "kits.yml");
 
+        KitManager.loadKits();
+        Bukkit.getPluginManager().registerEvents(new ButtonListener(), this);
+
         new GkitCommand();
         new ManageCommand();
+        new FrozedGkitsCommand();
+
+        this.getServer().getConsoleSender().sendMessage(Color.CHAT_BAR);
+        this.getServer().getConsoleSender().sendMessage(Color.translate("&b&lFrozedGKits &8- &7v") + getDescription().getVersion());
+        this.getServer().getConsoleSender().sendMessage(Color.translate("&b ▶ &aLoaded files: config.yml, kits.yml"));
+        this.getServer().getConsoleSender().sendMessage(Color.translate("&b ▶ &aCommands, Listeners & Managers registered."));
+        this.getServer().getConsoleSender().sendMessage(Color.CHAT_BAR);
     }
 
     @Override
     public void onDisable() {
-        
+        KitManager.saveKits();
+        instance = null;
     }
 }
