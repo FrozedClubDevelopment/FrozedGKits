31b00a94eba6e899118bbf2e20e57e638afc7353
Alex Avila <alex.ramon2004@gmail.com>
2020-09-27 00:44:31 -0500

    Slot menu

--- src/main/java/club/frozed/gkit/provider/edition/KitManagerSlotSelectionMenu.java
+++ src/main/java/club/frozed/gkit/provider/edition/KitManagerSlotSelectionMenu.java
@@ -1,20 +1,21 @@
 package club.frozed.gkit.provider.edition;
 
 import club.frozed.gkit.FrozedGKits;
+import club.frozed.gkit.kit.Kit;
 import club.frozed.gkit.kit.KitManager;
+import club.frozed.gkit.provider.selection.KitManagerSelectionMenu;
 import club.frozed.gkit.utils.chat.Color;
 import club.frozed.gkit.utils.items.ItemCreator;
 import club.frozed.gkit.utils.menu.Button;
 import club.frozed.gkit.utils.menu.Menu;
+import org.apache.commons.lang.StringUtils;
 import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.ClickType;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by Elb1to
@@ -22,6 +23,12 @@ import java.util.Map;
  * Date: 09/26/2020 @ 13:49
  */
 public class KitManagerSlotSelectionMenu extends Menu {
+
+    private Kit kitManager;
+
+    public KitManagerSlotSelectionMenu(Kit kitManager){
+        this.kitManager = kitManager;
+    }
 
     private final FrozedGKits plugin = FrozedGKits.getInstance();
 
@@ -34,57 +41,43 @@ public class KitManagerSlotSelectionMenu extends Menu {
     public Map<Integer, Button> getButtons(Player player) {
         Map<Integer, Button> buttons = new HashMap<>();
 
-        /*for (int i = 0; i < (9 * FrozedGKits.getInstance().getPluginConfig().getConfig().getInt("KIT-MANAGER-MENU.INVENTORY-SIZE")); i++) {
-            buttons.put(i, new KitSelectionActionButton(kitManager));
-        }*/
-
-        for (KitManager kitManager : KitManager.getKits()) {
-            for (int i = 0; i < (9 * FrozedGKits.getInstance().getPluginConfig().getConfig().getInt("KIT-MANAGER-MENU.INVENTORY-SIZE")); i++) {
-                buttons.put(i, new KitSelectionActionButton(kitManager));
-            }
+        for (Kit kitManager : KitManager.getKits()) {
+            buttons.put(kitManager.getSlotPosition(), new SlotButton(false,kitManager));
         }
+
+        setPlaceholder(true);
+        setPlaceholderButton(new SlotButton(true,kitManager));
 
         return buttons;
     }
 
-    public static class KitSelectionActionButton extends Button {
+    private static class SlotButton extends Button{
 
-        KitManager kitManager;
+        private boolean xd;
+
+        private final Kit kitSlot;
 
         @Override
         public ItemStack getButtonItem(Player player) {
-            int i = 0;
-            return new ItemCreator(Material.STAINED_GLASS_PANE)
-                    .setDurability(0)
-                    .setName(Color.translate("&b&lSlot #" + i))
-                    .setLore(Arrays.asList(
-                            Color.MENU_BAR,
-                            "&7This is slot is free",
-                            "&7Click to set the slot position",
-                            "&7for the " + kitManager.getColor() + kitManager.getName() + "&7 GKit here.",
-                            Color.MENU_BAR))
-                    .get();
+            return new ItemCreator(Material.STAINED_GLASS_PANE).setName(Color.translate(xd ? "&aAvailable" : "&cNot available")).setDurability(xd ? 5 : 14).get();
         }
 
         @Override
         public void clicked(Player player, int slot, ClickType clickType, int hotbarButton) {
-            if (clickType.isLeftClick()) {
-                player.playSound(player.getLocation(), Sound.SUCCESSFUL_HIT, 1.0F, 1.0F);
-
-                player.closeInventory();
-                player.sendMessage(Color.CHAT_BAR);
-                player.sendMessage(Color.translate("&8[&b&l!&8] &aType the name of the new kit..."));
-                player.sendMessage(Color.CHAT_BAR);
-            }
+            if (!xd) return;
+            kitSlot.setSlotPosition(slot);
+            playSuccess(player);
+            new KitManagerEditionMenu(kitSlot).openMenu(player);
         }
 
-        public KitSelectionActionButton(KitManager kit) {
-            this.kitManager = kit;
+        public SlotButton(boolean xd, Kit kitSlot) {
+            this.xd = xd;
+            this.kitSlot = kitSlot;
         }
     }
 
     @Override
     public int size(Map<Integer, Button> buttons) {
-        return 9 * plugin.getPluginConfig().getConfig().getInt("KIT-MANAGER-MENU.INVENTORY-SIZE");
+        return 9 * plugin.getPluginConfig().getConfig().getInt("KIT-SELECTION-MENU.INVENTORY-SIZE");
     }
 }
