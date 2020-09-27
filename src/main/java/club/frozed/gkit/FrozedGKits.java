bb6f984bdbf39a0b6c84c6152dee29cb3156aa9d
Elb1to <elbitomc@gmail.com>
2020-09-27 02:45:28 -0500

    Code cleanup

--- src/main/java/club/frozed/gkit/FrozedGKits.java
+++ src/main/java/club/frozed/gkit/FrozedGKits.java
@@ -5,7 +5,7 @@ import club.frozed.gkit.commands.GkitCommand;
 import club.frozed.gkit.commands.ManageCommand;
 import club.frozed.gkit.kit.KitCreationListener;
 import club.frozed.gkit.kit.KitManager;
-import club.frozed.gkit.provider.player.PlayerLoadListener;
+import club.frozed.gkit.provider.data.PlayerLoadListener;
 import club.frozed.gkit.utils.chat.Color;
 import club.frozed.gkit.utils.command.CommandFramework;
 import club.frozed.gkit.utils.config.FileConfig;
@@ -19,8 +19,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 @Setter
 public final class FrozedGKits extends JavaPlugin {
 
-    @Getter
-    private static FrozedGKits instance;
+    @Getter private static FrozedGKits instance;
     private CommandFramework commandFramework;
 
     private FileConfig pluginConfig, kitsConfig, dataConfig;
@@ -40,28 +39,7 @@ public final class FrozedGKits extends JavaPlugin {
         kitsConfig = new FileConfig(this, "kits.yml");
         dataConfig = new FileConfig(this, "data.yml");
 
-
         KitManager.loadKits();
-
-        /*
-         * TODO-LIST:
-         *      - Menu de selección de kit (para users) -> DOn3
-         *      - Menu de seleccion de color para el kit (para admin) -> DoN3
-         *      - Menu de seleccion de slot para el kit (para admin) -> Don3
-         *      - Hacer cooldown de kit
-         *      - Verificar que todos los menus funcionen 100%
-         */
-
-        /*if (!KitManager.getKits().isEmpty()) {
-            try {
-                KitManager.loadKits();
-            } catch (Exception exception) {
-                Bukkit.getConsoleSender().sendMessage(Color.CHAT_BAR);
-                Bukkit.getConsoleSender().sendMessage(Color.translate("&cAn error occurred while trying to load your kits."));
-                Bukkit.getConsoleSender().sendMessage(Color.translate("&cPlease check your kits.yml file!"));
-                Bukkit.getConsoleSender().sendMessage(Color.CHAT_BAR);
-            }
-        }*/
 
         Bukkit.getPluginManager().registerEvents(new ButtonListener(), this);
         Bukkit.getPluginManager().registerEvents(new KitCreationListener(), this);
